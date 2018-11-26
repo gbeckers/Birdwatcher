4749bcdb5a7779be6cc4c2c5e1099f78a6b70eb8
Gabriel Beckers <gabriel@gbeckers.nl>
2018-11-26 15:56:09 +0100

    added test for iter_frames

--- birdwatcher/tests/test_videoinput.py
+++ birdwatcher/tests/test_videoinput.py
@@ -6,5 +6,12 @@ class TestVideos(unittest.TestCase):
 
     def test_testvideosmall(self):
         vf = bw.testvideosmall()
+        self.assertEqual(vf.framecount, 497)
+
+    def test_videoiterframes(self):
+        vf = bw.testvideosmall()
+        shape = (vf.shape[1], vf.shape[0], 3)
+        for frame in vf.iter_frames():
+            self.assertEqual(frame.shape, shape)
 
 
