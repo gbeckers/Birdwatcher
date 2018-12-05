019bfa3744181d22ea4d0b7737e941085951ec12
Gabriel Beckers <gabriel@gbeckers.nl>
2018-12-05 12:21:53 +0100

    completed docstring of arraytovideo

--- birdwatcher/ffmpeg.py
+++ birdwatcher/ffmpeg.py
@@ -6,7 +6,8 @@ from .utils import peek_iterable
 __all__ = ['arraytovideo']
 
 def arraytovideo(frames, filename, framerate, crf=17, format='mp4',
-                 codec='libx264', ffmpegpath='ffmpeg', report_args=False):
+                 codec='libx264', pixfmt='yuv420p', ffmpegpath='ffmpeg',
+                 report_args=False):
     """Writes an iterable of numpy frames as video file using ffmpeg.
 
     Parameters
@@ -16,9 +17,17 @@ def arraytovideo(frames, filename, framerate, crf=17, format='mp4',
         values ranging from 0 to 255.
     filename: str
         Name of the videofile that should be written to
+    framerate: int
+        framerate of video in frames per second
     crf: int
         Value determines quality of video. Default: 17, which is high
         quality. See ffmpeg documentation.
+    format: str
+        ffmpeg video format. Default is 'mp4'. See ffmpeg documentation.
+    codec: str
+        ffmpeg video codec. Default is 'libx264'. See ffmpeg documentation.
+    pixfmt: str
+        ffmpeg pixel format. Default is 'yuv420p'. See ffmpeg documentation.
     ffmpegpath: str or pathlib.Path
         Path to ffmpeg executable. Default is `ffmpeg`, which means it
         should be in the system path.
@@ -32,7 +41,7 @@ def arraytovideo(frames, filename, framerate, crf=17, format='mp4',
             '-s', f'{width}x{height}', '-i', 'pipe:',
             '-vcodec', f'{codec}',
             '-f', f'{format}', '-crf', f'{crf}',
-            '-pix_fmt', 'yuv420p',
+            '-pix_fmt', f'{pixfmt}',
             filename, '-y']
     if report_args:
         print(args)
