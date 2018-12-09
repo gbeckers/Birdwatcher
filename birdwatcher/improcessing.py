b0e6ff3b4844f0f0a13cbe4a74f172ffe18fb260
Gabriel Beckers <gabriel@gbeckers.nl>
2018-12-09 08:35:06 +0100

    add docstrings improcessing

--- birdwatcher/improcessing.py
+++ birdwatcher/improcessing.py
@@ -4,33 +4,108 @@ import cv2 as cv
 __all__ = ['framesgray', 'framescolor', 'draw_circles']
 
 
-def _emptyframegray(height, width, value=0, dtype='uint8'):
+def framegray(height, width, value=0, dtype='uint8'):
+    """Creates a gray frame.
+
+    Parameters
+    ----------
+    height: int
+        Height of frame.
+    width: int
+        Width of frame.
+    value:
+        Fill value of frame. Default 0 (black).
+    dtype: numpy dtype
+        Dtype of frame. Default `uint8'
+
+    Returns
+    -------
+    numpy ndarray
+
+    """
     return np.ones((height, width), dtype=dtype) * value
 
 
-def _emptyframecolor(height, width, color=(0, 0, 0), dtype='uint8'):
+def framecolor(height, width, color=(0, 0, 0), dtype='uint8'):
+    """Creates a color frame.
+
+        Parameters
+        ----------
+        height: int
+            Height of frame.
+        width: int
+            Width of frame.
+        color:
+            Fill value of frame. Default (0, 0, 0) (black).
+        dtype: numpy dtype
+            Dtype of frame. Default `uint8'
+
+        Returns
+        -------
+        numpy ndarray
+
+        """
     return np.ones((height, width, 3), dtype=dtype) * np.asanyarray(color,
                                                                     dtype=dtype)
 
 
 def framesgray(nframes, height, width, value=0, dtype='uint8'):
-    frame = _emptyframegray(height=height, width=width, value=value,
-                            dtype=dtype)
+    """Creates an iterator that yields gray frames.
+
+    Parameters
+    ----------
+    nframes: int
+        Number of frames to be produced.
+    height: int
+        Height of frame.
+    width: int
+        Width of frame.
+    value:
+        Fill value of frame. Default 0 (black).
+    dtype: numpy dtype
+        Dtype of frame. Default `uint8'
+
+    Returns
+    -------
+    Iterator of numpy ndarrays
+
+    """
+    frame = framegray(height=height, width=width, value=value,
+                      dtype=dtype)
     for i in range(nframes):
         yield frame.copy()
 
 
 def framescolor(nframes, height, width, color=(0, 0, 0), dtype='uint8'):
-    frame = _emptyframecolor(height=height, width=width, color=color,
-                             dtype=dtype)
+    """Creates an iterator that yields color frames.
+
+        Parameters
+        ----------
+        nframes: int
+            Number of frames to be produced.
+        height: int
+            Height of frame.
+        width: int
+            Width of frame.
+        color:
+            Fill value of frame. Default (0, 0, 0) (black).
+        dtype: numpy dtype
+            Dtype of frame. Default `uint8'
+
+        Returns
+        -------
+        Iterator of numpy ndarrays
+
+        """
+    frame = framecolor(height=height, width=width, color=color,
+                       dtype=dtype)
     for i in range(nframes):
         yield frame.copy()
 
 
 def draw_circles(frames, centers, radius=4, color=(255, 100, 0), thickness=2,
                  linetype=8, shift=0):
-    """Creates a frame iterator that draws circles on the frames in a
-    frame iterable.
+    """Creates a frame iterator that draws circles on an input frames iterable.
 
     Frames and centers should be iterables that have the same length.
 
