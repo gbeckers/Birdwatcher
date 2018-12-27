8cf0eb6e03f4804e1638656dbee9c33ec3799a23
Gabriel Beckers <gabriel@gbeckers.nl>
2018-12-27 15:12:35 +0100

    import some util functions

--- birdwatcher/__init__.py
+++ birdwatcher/__init__.py
@@ -1,8 +1,9 @@
-from .videoinput import *
+from .video import *
 from .movementdetection import *
 from .backgroundsubtraction import *
 from .coordinatearrays import *
 from .frameprocessing import *
+from .utils import *
 #from .plotting import *
 from ._version import get_versions
 __version__ = get_versions()['version']
