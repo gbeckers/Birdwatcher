c39fc8d5ed4732bd38999e9161e43aff289d4b5a
Gabriel Beckers <gabriel@gbeckers.nl>
2018-12-27 15:03:21 +0100

    more tests videoinput

--- birdwatcher/tests/test_videoinput.py
+++ birdwatcher/tests/test_videoinput.py
@@ -6,14 +6,20 @@ class TestVideos(unittest.TestCase):
 
     def test_videoiterframes(self):
         vf = bw.testvideosmall()
-        shape = (vf.shape[1], vf.shape[0], 3)
-        for i,frame in enumerate(vf.iter_frames()):
+        shape = (vf.frameheight, vf.framewidth, 3)
+        for i,frame in enumerate(vf.iter_frames(), 1):
             self.assertEqual(frame.shape, shape)
-        self.assertEqual(i, 496) # 497 frames
+        self.assertEqual(i, 497)
 
-    def test_nframes(self):
+    def test_size(self):
         vf = bw.testvideosmall()
-        self.assertEqual(vf.nframes, 497)
+        self.assertEqual(vf.framewidth, 1280)
+        self.assertEqual(vf.frameheight, 720)
+        self.assertSequenceEqual(vf.framesize, (1280, 720))
+
+    def test_countframes(self):
+        vf = bw.testvideosmall()
+        self.assertEqual(vf.count_frames(), 497)
 
 
 
