07d5132121b7d2daaded481f76a095d8a1b5e41d
Gabriel Beckers <gabriel@gbeckers.nl>
2022-02-20 15:29:36 +0100

    test shorter videos

--- birdwatcher/tests/test_movementdetection.py
+++ birdwatcher/tests/test_movementdetection.py
@@ -64,37 +64,44 @@ class TestDetectMovement(unittest.TestCase):
 
     def setUp(self):
         self.tempdirname1 = Path(tempfile.mkdtemp())
+        self.vfs = (bw.testvideosmall()
+                      .iter_frames(nframes=20)
+                      .tovideo(self.tempdirname1 / 'even1.mp4', framerate=25))
 
     def tearDown(self):
         shutil.rmtree(self.tempdirname1)
 
     def test_MOG2(self):
         bgs = bw.BackgroundSubtractorMOG2(History=2)
-        bw.detect_movement(bw.testvideosmall(), bgs=bgs,
+        bw.detect_movement(self.vfs, bgs=bgs,
                            analysispath=self.tempdirname1, overwrite=True)
 
     def test_KNN(self):
         bgs = bw.BackgroundSubtractorKNN(History=2)
-        bw.detect_movement(bw.testvideosmall(), bgs=bgs,
+        bw.detect_movement(self.vfs, bgs=bgs,
                            analysispath=self.tempdirname1, overwrite=True)
 
     def test_LSBP(self):
         bgs = bw.BackgroundSubtractorLSBP(History=2)
-        bw.detect_movement(bw.testvideosmall(), bgs=bgs,
+        bw.detect_movement(self.vfs, bgs=bgs,
                            analysispath=self.tempdirname1,
                            overwrite=True)
 
 
 class TestBatchDetectMovement(unittest.TestCase):
+
     def setUp(self):
         self.tempdirname1 = Path(tempfile.mkdtemp())
+        self.vfs = (bw.testvideosmall()
+                    .iter_frames(nframes=20)
+                    .tovideo(self.tempdirname1 / 'even1.mp4', framerate=25))
 
     def tearDown(self):
          shutil.rmtree(self.tempdirname1)
 
     def test_batchdetection(self):
-        p1 = bw.testvideosmall()
-        p2 = p1.iter_frames().tovideo(self.tempdirname1 / 'even.mp4',
+        p1 = self.vfs
+        p2 = p1.iter_frames().tovideo(self.tempdirname1 / 'even2.mp4',
                                       framerate=p1.avgframerate)
         bgs = bw.BackgroundSubtractorKNN(History=2)
         bw.batch_detect_movement([p1,p2], bgs=bgs,
