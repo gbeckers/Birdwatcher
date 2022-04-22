4f48d84c8789095be3af7bb4625bde4124113174
Gabriel Beckers <gabriel@gbeckers.nl>
2022-04-22 14:57:49 +0200

    add test for peek_frames method

--- birdwatcher/tests/test_frameprocessing.py
+++ birdwatcher/tests/test_frameprocessing.py
@@ -1,5 +1,5 @@
 import unittest
-from birdwatcher.frames import FramesColor
+from birdwatcher.frames import Frames, FramesColor, framecolor
 
 
 class TestFrameColor(unittest.TestCase):
@@ -17,4 +17,19 @@ class TestFrameIterator(unittest.TestCase):
     def test_drawframenumber(self):
         for frame in self.colorframes.draw_framenumbers():
             self.assertGreater(frame.sum(), 0)
+
+class TestPeekFrame(unittest.TestCase):
+
+    def setUp(self):
+        self.frame0 = framecolor(width=640, height=480, color=(0,0,0))
+        self.frame1 = framecolor(width=640, height=480, color=(1,1,1))
+
+    def test_peekframe(self):
+        frames = Frames([self.frame0, self.frame1])
+        frame0 = frames.peek_frame()
+        self.assertEqual(frame0.sum(), self.frame0.sum())
+        framelist = [frame for frame in frames]
+        self.assertEqual(len(framelist), 2) # is the first frame still available?
+        self.assertEqual(framelist[1].sum(), self.frame1.sum())
+
 
