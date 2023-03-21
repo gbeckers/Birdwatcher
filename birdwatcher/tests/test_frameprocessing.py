be0a2b407fa955982c856fc4f45a357eb789fd7e
Carien Mol <c.mol@uu.nl>
2023-03-21 13:08:17 +0100

    add test for frameheight and framewidth

--- birdwatcher/tests/test_frameprocessing.py
+++ birdwatcher/tests/test_frameprocessing.py
@@ -11,11 +11,15 @@ graylist = [framegray(width=640, height=480, value=0),
             framegray(width=640, height=480, value=1)]
 
 
+class TestFrames(unittest.TestCase):
 
-class TestFrameColor(unittest.TestCase):
+    def test_size(self):
+        frames = Frames(colorlist)
+        self.assertEqual(frames.frameheight, 480)
+        self.assertEqual(frames.framewidth, 640)
 
     def test_color(self):
-        for frame in FramesColor(5, height=480, width=640, color=(0,0,0)):
+        for frame in FramesColor(5, height=480, width=640, color= (0,0,0)):
             self.assertEqual(frame.sum(), 0)
 
 
