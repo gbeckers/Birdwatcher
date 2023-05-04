f55f7c0d32a79bfde37572ee22997274a7e9907d
Carien Mol <c.mol@uu.nl>
2023-05-04 12:21:04 +0200

    reorder parameter module

--- birdwatcher/parameters.py
+++ birdwatcher/parameters.py
@@ -25,101 +25,6 @@ def get_all_combinations(**kwargs):
     return list(product_dict(**kwargs))
 
 
-def apply_all_parameters(vfs, settings, bgs_type=BackgroundSubtractorMOG2, 
-                         startat=None, duration=None):
-    """Run movement detection with each set of parameters.
-    
-    Parameters
-    ----------
-    vfs : VideoFileStream
-        A Birdwatcher VideoFileStream object.
-    settings : dict
-        Dictionary with parameter settings from the BackgroundSubtractor and 
-        settings for applying color, resizebyfactor, blur and morphologyex 
-        manipulations.
-    bge_type: BackgroundSubtractor
-        This can be any of the BackgroundSubtractors in Birdwatcher, e.g. 
-        BackgroundSubtractorMOG2, BackgroundSubtractorKNN, 
-        BackgroundSubtractorLSBP.
-    startat : str, optional
-        If specified, start at this time point in the video file. You can use 
-        two different time unit formats: sexagesimal 
-        (HOURS:MM:SS.MILLISECONDS, as in 01:23:45.678), or in seconds.
-    duration : int, optional
-        Duration of video fragment in seconds.
-    
-    """
-    nframes = vfs.avgframerate*duration if duration else None
-    
-    list_with_dfs = []
-
-    for setting in product_dict(**settings):
-        
-        frames = vfs.iter_frames(startat=startat, nframes=nframes, 
-                                 color=setting['color'])
-        
-        if setting['resizebyfactor'] != 1:
-            val = setting['resizebyfactor']
-            frames = frames.resizebyfactor(val,val)
-        
-        if setting['blur']:
-            val = setting['blur']
-            frames = frames.blur((val,val))
-        
-        # extract bgs settings and apply bgs
-        bgs_params = bgs_type().get_params()
-        bgs_settings = {p:setting[p] for p in bgs_params.keys()}
-        bgs = bgs_type(**bgs_settings)
-        frames = frames.apply_backgroundsegmenter(bgs, learningRate=-1)
-        
-        if settings['morphologyex']:
-            frames = frames.morphologyex(morphtype='open', kernelsize=2)
-        
-        # find mean of nonzero coordinates
-        coordinates = frames.find_nonzero()
-        coordsmean = np.array([c.mean(0) if c.size>0 else (np.nan, np.nan) for 
-                               c in coordinates])
-
-        # save coordsmean x,y in pandas DataFrame 
-        # with associated settings as column labels
-        setting['coords'] = ['x', 'y']
-        columns = pd.MultiIndex.from_frame(pd.DataFrame(setting))
-        df = pd.DataFrame(coordsmean, columns=columns)
-        list_with_dfs.append(df)
-        
-    df = pd.concat(list_with_dfs, axis=1)
-    
-    # create long-format
-    df.index.name = 'framenumber'
-    df = (df.stack(list(range(df.columns.nlevels)), dropna=False)
-          .reset_index()  # stack all column levels
-          .rename({0: 'pixel'}, axis=1))
-    
-    return ParameterSelection(df, vfs.filepath, str(bgs_type), startat, duration)
-
-def load_parameterselection(path):
-    """Load a parameterselection.csv file.
-    
-    Parameters
-    ----------
-    path : str
-        Name of the directory where parameterselection.csv is saved.
-    
-    Returns
-    ------
-    ParameterSelection
-    
-    """
-    
-    filepath = Path(path) / 'parameterselection.csv' 
-    df = pd.read_csv(filepath, index_col=0, engine='python')
-    info = eval(df.index.names[0])
-    df.index.name = None
-    
-    return ParameterSelection(df, info['vfs'], info['bgs_type'], 
-                              info['startat'], info['duration'], path)
-
-
 class ParameterSelection():
     """A Pandas dataframe with movement detection results of various parameter 
     settings associated with a (fragment of a) Videofilestream.
@@ -186,30 +91,6 @@ class ParameterSelection():
         else:
             raise Exception(f"'{selection}' is not recognized. Please "
                             "choose between 'all' and 'multi_only'.")
-
-    def select_data(self, **kwargs):
-        """Returns a copy with a selection of the dataframe.
-        
-        **kwargs can be a dictionary, with keys matching column names. The
-        value of each key will be selected from a copy of the dataframe.
-        
-        """
-        
-        df = self.df.copy()
-        for key, value in kwargs.items():
-            df = df.loc[df[key]==value]
-        
-        return df
-    
-    def _check_multi_only(self, inputdict):
-        """Check if all parameters that are tested with multiple settings, are 
-        included in the inputdic.
-        
-        """
-        multi_only = self.get_parameters('multi_only')
-        if multi_only.keys() != inputdict.keys():
-            raise Exception("Make sure the input dictionary contains all keys"
-                            f" with multiple values: {multi_only.keys()}")
 
     def plot_parameters(self, rows, cols, default_values):
         """Returns a figure from seaborn with subplots.
@@ -404,3 +285,122 @@ class ParameterSelection():
         Path(path).mkdir(parents=True, exist_ok=overwrite)
 
         return path
+    
+    def select_data(self, **kwargs):
+        """Returns a copy with a selection of the dataframe.
+        
+        **kwargs can be a dictionary, with keys matching column names. The
+        value of each key will be selected from a copy of the dataframe.
+        
+        """
+        
+        df = self.df.copy()
+        for key, value in kwargs.items():
+            df = df.loc[df[key]==value]
+        
+        return df
+    
+    def _check_multi_only(self, inputdict):
+        """Check if all parameters that are tested with multiple settings, are 
+        included in the inputdic.
+        
+        """
+        multi_only = self.get_parameters('multi_only')
+        if multi_only.keys() != inputdict.keys():
+            raise Exception("Make sure the input dictionary contains all keys"
+                            f" with multiple values: {multi_only.keys()}")
+
+
+def apply_all_parameters(vfs, settings, bgs_type=BackgroundSubtractorMOG2, 
+                         startat=None, duration=None):
+    """Run movement detection with each set of parameters.
+    
+    Parameters
+    ----------
+    vfs : VideoFileStream
+        A Birdwatcher VideoFileStream object.
+    settings : dict
+        Dictionary with parameter settings from the BackgroundSubtractor and 
+        settings for applying color, resizebyfactor, blur and morphologyex 
+        manipulations.
+    bge_type: BackgroundSubtractor
+        This can be any of the BackgroundSubtractors in Birdwatcher, e.g. 
+        BackgroundSubtractorMOG2, BackgroundSubtractorKNN, 
+        BackgroundSubtractorLSBP.
+    startat : str, optional
+        If specified, start at this time point in the video file. You can use 
+        two different time unit formats: sexagesimal 
+        (HOURS:MM:SS.MILLISECONDS, as in 01:23:45.678), or in seconds.
+    duration : int, optional
+        Duration of video fragment in seconds.
+    
+    """
+    nframes = vfs.avgframerate*duration if duration else None
+    
+    list_with_dfs = []
+
+    for setting in product_dict(**settings):
+        
+        frames = vfs.iter_frames(startat=startat, nframes=nframes, 
+                                 color=setting['color'])
+        
+        if setting['resizebyfactor'] != 1:
+            val = setting['resizebyfactor']
+            frames = frames.resizebyfactor(val,val)
+        
+        if setting['blur']:
+            val = setting['blur']
+            frames = frames.blur((val,val))
+        
+        # extract bgs settings and apply bgs
+        bgs_params = bgs_type().get_params()
+        bgs_settings = {p:setting[p] for p in bgs_params.keys()}
+        bgs = bgs_type(**bgs_settings)
+        frames = frames.apply_backgroundsegmenter(bgs, learningRate=-1)
+        
+        if settings['morphologyex']:
+            frames = frames.morphologyex(morphtype='open', kernelsize=2)
+        
+        # find mean of nonzero coordinates
+        coordinates = frames.find_nonzero()
+        coordsmean = np.array([c.mean(0) if c.size>0 else (np.nan, np.nan) for 
+                               c in coordinates])
+
+        # save coordsmean x,y in pandas DataFrame 
+        # with associated settings as column labels
+        setting['coords'] = ['x', 'y']
+        columns = pd.MultiIndex.from_frame(pd.DataFrame(setting))
+        df = pd.DataFrame(coordsmean, columns=columns)
+        list_with_dfs.append(df)
+        
+    df = pd.concat(list_with_dfs, axis=1)
+    
+    # create long-format
+    df.index.name = 'framenumber'
+    df = (df.stack(list(range(df.columns.nlevels)), dropna=False)
+          .reset_index()  # stack all column levels
+          .rename({0: 'pixel'}, axis=1))
+    
+    return ParameterSelection(df, vfs.filepath, str(bgs_type), startat, 
+                              duration)
+
+def load_parameterselection(path):
+    """Load a parameterselection.csv file.
+    
+    Parameters
+    ----------
+    path : str
+        Name of the directory where parameterselection.csv is saved.
+    
+    Returns
+    ------
+    ParameterSelection
+    
+    """
+    filepath = Path(path) / 'parameterselection.csv' 
+    df = pd.read_csv(filepath, index_col=0, engine='python')
+    info = eval(df.index.names[0])
+    df.index.name = None
+    
+    return ParameterSelection(df, info['vfs'], info['bgs_type'], 
+                              info['startat'], info['duration'], path)
