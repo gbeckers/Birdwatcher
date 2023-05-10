0efaa1d898910ebcf65965b7f3bf2fdcdf7bd9a1
Carien Mol <c.mol@uu.nl>
2023-05-10 10:49:40 +0200

    clean docstrings/comments and check PEP8

--- birdwatcher/parameters.py
+++ birdwatcher/parameters.py
@@ -26,7 +26,7 @@ class ParameterSelection():
               ('lime', [0, 255, 0]),
               ('cyan', [255, 255, 0]),
               ('magenta', [255, 0, 255])]
-    
+
     def __init__(self, df, videofilepath, bgs_type, startat, duration, 
                  path=None):
         self.df = df
@@ -69,7 +69,6 @@ class ParameterSelection():
         """
         paramkeys = (set(self.df.columns) - 
                      set(['framenumber', 'pixel', 'coords']))
-        
         all_parameters = {k:list(self.df[k].unique()) for k in paramkeys}
         
         if selection == 'all':
@@ -90,9 +89,9 @@ class ParameterSelection():
         Parameters
         ----------
         rows : str
-            One parameter from tested with multiple values.
+            One parameter tested with multiple values.
         cols : str
-            A second parameter with multiple values.
+            A second parameter tested with multiple values.
         default_values : dict
             All parameters that are tested with multiple settings, should be 
             added to a dictionary with each parameter as key, and the default 
@@ -107,14 +106,14 @@ class ParameterSelection():
         self._check_multi_only(default_values)
         other_values = {key:default_values[key] for key in 
                         default_values.keys()-[rows, cols]}
-        df_selection = self.select_data(**other_values)
+        df_selection = self._select_data(**other_values)
 
         # plot with seaborn
         g = sns.relplot(x="framenumber", y="pixel", hue="coords", style=None, 
                         col=cols, row=rows, kind="line", data=df_selection, 
                         height=3, aspect=2)
         g.figure.suptitle(str(other_values), fontsize=15, x=0.51, y=1.05)
-
+        
         return g
 
     def batch_plot_parameters(self, default_values, overwrite=False):
@@ -137,7 +136,7 @@ class ParameterSelection():
             to the foldername, if the filepath already exists.
         
         """            
-        path = self.create_path(self.path, 'figures', overwrite)
+        path = self._create_path(self.path, 'figures', overwrite)
         
         # save default values as txt file
         with open(path / 'default_values.txt', 'w') as f:
@@ -146,19 +145,13 @@ class ParameterSelection():
         
         # plot and save each combination of two parameters
         settings = self.get_parameters('multi_only')
-        
         for rows, _ in settings.items():
             for cols, _ in settings.items():
                 if rows != cols:
-                    g = self.plot_parameters(rows, cols, default_values)
-                    
-                    # save figure
                     filename = f'{self.vfs.filepath.stem}_{rows}_{cols}.png'
+                    g = self.plot_parameters(rows, cols, default_values)
                     g.savefig(path / filename)
-                    
-                    # close figure
                     plt.close(g.figure)
-                    
         print(f"The figures are saved in {path}")
 
     def draw_multiple_circles(self, settings, radius=60, thickness=2):
@@ -187,24 +180,18 @@ class ParameterSelection():
         """
         self._check_multi_only(settings)
         self._check_number_of_colorcombinations(settings)
-        
+
         frames = self.get_videofragment().draw_framenumbers()
         colorspecs = {}
-        
         for i, setting in enumerate(product_dict(**settings)):
             colorspecs[self.colors[i][0]] = setting
-            
-            # select data
-            df_selection = self.select_data(**setting)
-            
-            # transform into iterable
+            df_selection = self._select_data(**setting)
             iterdata = (df_selection.set_index(['framenumber', 'coords'])
                         .loc[:, 'pixel'].unstack().values)
-            
             frames = frames.draw_circles(iterdata, radius=radius,
                                          color=self.colors[i][1],
                                          thickness=thickness)
-            
+
         return frames, pd.DataFrame(colorspecs)
 
     def save_parameters(self, path, foldername=None, overwrite=False):
@@ -228,18 +215,14 @@ class ParameterSelection():
         """
         if foldername is None:
             foldername = f'params_{self.vfs.filepath.stem}'
-        path = self.create_path(path, foldername, overwrite)
-        
-        # add object information to dataframe
-        json_info = json.dumps(self.get_info())
-        
-        # save as .csv file
-        self.df.to_csv(path / 'parameterselection.csv', index_label=json_info)
-        
-        # return path information
+        path = self._create_path(path, foldername, overwrite)
         self.path = str(path)
+        
+        # add extra information to saved .csv file
+        json_info = json.dumps(self.get_info())
+        self.df.to_csv(path / 'parameterselection.csv', index_label=json_info)
 
-    def create_path(self, path, foldername, overwrite):
+    def _create_path(self, path, foldername, overwrite):
         """Useful for creating a path with a number added as suffix in case 
         the folder already exists.
 
@@ -255,31 +238,29 @@ class ParameterSelection():
 
         """
         path = Path(path) / foldername
-
+        
         if not overwrite:
             i = 1
             while path.exists():
                 i += 1
                 path = path.parent / f'{foldername}_{i}'
-
         Path(path).mkdir(parents=True, exist_ok=overwrite)
-
+        
         return path
-    
-    def select_data(self, **kwargs):
+
+    def _select_data(self, **kwargs):
         """Returns a copy with a selection of the dataframe.
         
         **kwargs can be a dictionary, with keys matching column names. The
         value of each key will be selected from a copy of the dataframe.
         
         """
-        
         df = self.df.copy()
         for key, value in kwargs.items():
             df = df.loc[df[key]==value]
-        
+            
         return df
-    
+
     def _check_multi_only(self, inputdict):
         """Check if all parameters that are tested with multiple settings, are 
         included in the inputdic.
@@ -289,7 +270,7 @@ class ParameterSelection():
         if multi_only.keys() != inputdict.keys():
             raise Exception("Make sure the input dictionary contains all keys"
                             f" with multiple values: {multi_only.keys()}")
-    
+
     def _check_number_of_colorcombinations(self, settings):
         """Check if the number of settings combinations does not exceed the
         number of possible colors.
@@ -331,23 +312,21 @@ def apply_all_parameters(vfs, settings, bgs_type=BackgroundSubtractorMOG2,
     
     """
     nframes = vfs.avgframerate*duration if duration else None
-    
     list_with_dfs = []
-
+    
     for setting in product_dict(**settings):
-        
         frames = vfs.iter_frames(startat=startat, nframes=nframes, 
                                  color=setting['color'])
-        
+
         if setting['resizebyfactor'] != 1:
             val = setting['resizebyfactor']
             frames = frames.resizebyfactor(val,val)
-        
+
         if setting['blur']:
             val = setting['blur']
             frames = frames.blur((val,val))
         
-        # extract bgs settings and apply bgs
+        # extract and apply bgs settings
         bgs_params = bgs_type().get_params()
         bgs_settings = {p:setting[p] for p in bgs_params.keys()}
         bgs = bgs_type(**bgs_settings)
@@ -361,16 +340,14 @@ def apply_all_parameters(vfs, settings, bgs_type=BackgroundSubtractorMOG2,
         coordsmean = np.array([c.mean(0) if c.size>0 else (np.nan, np.nan) for 
                                c in coordinates])
 
-        # save coordsmean x,y in pandas DataFrame 
-        # with associated settings as column labels
+        # add results as pandas DataFrame
         setting['coords'] = ['x', 'y']
         columns = pd.MultiIndex.from_frame(pd.DataFrame(setting))
         df = pd.DataFrame(coordsmean, columns=columns)
         list_with_dfs.append(df)
-        
-    df = pd.concat(list_with_dfs, axis=1)
     
-    # create long-format
+    # create long-format DataFrame
+    df = pd.concat(list_with_dfs, axis=1)
     df.index.name = 'framenumber'
     df = (df.stack(list(range(df.columns.nlevels)), dropna=False)
           .reset_index()  # stack all column levels
