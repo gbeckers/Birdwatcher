38d09a544e6efeb773a41f455a5f393e8ea5a0c3
Carien Mol <c.mol@uu.nl>
2023-05-23 12:23:57 +0200

    remove import recording module from init file

--- birdwatcher/__init__.py
+++ birdwatcher/__init__.py
@@ -4,15 +4,8 @@ from .coordinatearrays import *
 from .frames import *
 from .utils import *
 
-try:
-    import pypylon
-    from .recording import *
-except ImportError:
-    pass
-
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
-
 
 from .tests import test
