e7b551cfbde7a7fbdc0bc658e15d77e5f5bfb009
Carien Mol <c.mol@uu.nl>
2023-06-01 10:22:35 +0200

    add tests for parameterselection

--- birdwatcher/tests/__init__.py
+++ birdwatcher/tests/__init__.py
@@ -1,19 +1,21 @@
 from unittest import TestLoader, TextTestRunner, TestSuite
 
-from . import test_coordinatearrays
-from . import test_frameprocessing
-from . import test_movementdetection
-from . import test_plotting
 from . import test_videoinput
+from . import test_frameprocessing
 from . import test_backgroundsubtraction
+from . import test_coordinatearrays
+from . import test_plotting
+from . import test_movementdetection
+from . import test_parameterselection
 
 
-modules = [test_coordinatearrays, test_frameprocessing,
-           test_movementdetection, test_plotting, test_videoinput, 
-           test_backgroundsubtraction]
+modules = [test_videoinput, test_frameprocessing, test_backgroundsubtraction, 
+           test_coordinatearrays, test_plotting, test_movementdetection, 
+           test_parameterselection]
+
 
 def test(verbosity=1):
-    suite =TestSuite()
+    suite = TestSuite()
     for module in modules:
         suite.addTests(TestLoader().loadTestsFromModule(module))
     return TextTestRunner(verbosity=verbosity).run(suite)
